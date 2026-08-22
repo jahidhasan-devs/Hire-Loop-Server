d8e602fe66168f3f60dead4775466c72c67f919b
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-08-23 04:42:08 +0600

    backend server and connect with database and fontend table design

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const app = express();
+const cors=require('cors');
+const port = 5000;
+app.use(cors());
+require("dotenv").config();
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+
+
+
+const uri = process.env.MONGO_DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+     const database = client.db("hireloop");
+     const jobCollection = database.collection("jobs");
+     const companyCollection=database.collection("companies");
+
+ app.post('/api/jobs',async(req,res)=>{
+     const job=req.body;
+     const result=await jobCollection.insertOne(job);
+     res.send(result);
+ })
+
+ app.get("/api/jobs",async(req,res)=>{
+    const query={};
+    if(req.query.companyId){
+        query['company.id']=req.query.companyId;
+    }
+    if(req.query.status){
+        query.status=req.query.status;
+    }
+    const cursor=jobCollection.find(query);
+    const result =await cursor.toArray();
+    res.send(result);
+
+ })
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
