6c1beefeb692d7311208f68b40103ca48f2b2827
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-09-03 02:11:11 +0600

    searching  api created

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors());
 require("dotenv").config();
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -35,11 +35,51 @@ async function run() {
      const database = client.db("hireloop");
      const jobCollection = database.collection("jobs");
      const companyCollection=database.collection("companies");
+     const usersCollection=database.collection("user");
 
- app.post('/api/jobs',async(req,res)=>{
+     app.get('/api/users', async(req,res)=>{
+      const cursor=usersCollection.find()
+      const result=await cursor.toArray();
+      res.send(result);
+     })
+
+    app.post('/api/jobs',async(req,res)=>{
      const job=req.body;
-     const result=await jobCollection.insertOne(job);
+     const newJob={
+      ...job,
+      createdAt:new Date()
+     }
+     const result=await jobCollection.insertOne(newJob);
      res.send(result);
+ })
+
+ //company related apis
+  
+ app.get('/api/companies', async(req,res)=>{
+  const cursor=companyCollection.find().skip(4);
+  const result=await cursor.toArray();
+  res.send(result)
+ })
+
+ //company related api
+ app.post('/api/companies',async(req,res)=>{
+  const company=req.body;
+  const newCompany={
+    ...company,
+    createdAt:new Date()
+  }
+  const result = await companyCollection.insertOne(newCompany);
+  res.send(result)
+ })
+
+ //company related apis
+ app.get('/api/my/companies',async(req,res)=>{
+  const query={};
+  if(req.query.recruiterId){
+    query.recruiterId=req.query.recruiterId;
+  }
+  const result=await companyCollection.findOne(query);
+  res.send(result || {})
  })
 
  app.get("/api/jobs",async(req,res)=>{
@@ -50,9 +90,17 @@ async function run() {
     if(req.query.status){
         query.status=req.query.status;
     }
-    const cursor=jobCollection.find(query);
+    const cursor=jobCollection.find(query).skip(12);
     const result =await cursor.toArray();
     res.send(result);
+
+ })
+
+ app.get('/api/jobs/:id',async (req,res)=>{
+  const id=req.params.id;
+  const query={_id:new ObjectId(id)};
+  const result=await jobCollection.findOne(query);
+  res.send(result);
 
  })
 
